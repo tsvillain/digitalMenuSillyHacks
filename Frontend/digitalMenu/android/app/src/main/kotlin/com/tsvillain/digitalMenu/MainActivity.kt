35a4965b36dcc98d1c2f5390db5a1586c4cbb6bf
Tekeshwar Singh <tekeshwarsingh2000@gmail.com>
2020-09-18 20:30:11 +0530

    🎉 initial commit"

---
+++ Frontend/digitalMenu/android/app/src/main/kotlin/com/tsvillain/digitalMenu/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tsvillain.digitalMenu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Frontend/digitalMenuCustomer/android/app/src/main/kotlin/com/tsvillain/digitalMenuCustomer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tsvillain.digitalMenuCustomer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
